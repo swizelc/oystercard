c507a6edd83c25acd929cde243c79e7924be0231
swizel <swizel.sarah@gmail.com>
2020-10-19 15:11:43 +0100

    challenge 4 complete

---
+++ lib/oystercard.rb
@@ -0,0 +1,8 @@
+class Osytercard
+  attr_reader :balance
+
+  def initialize(balance=0)
+    @balance = balance
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
-
+require 'oystercard'
 describe Osytercard do
+  it { is_expected.to respond_to(:balance) }
+
+  describe '.balance' do
+    it 'returns the balance of the card' do
+      expect(subject.balance).to eq 0
+    end
+  end
 
 end
